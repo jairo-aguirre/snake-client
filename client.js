cc441138bdfed6b7e1a39844766a59ddda9de7d7
Jairo (Burnaby, BC) <jairo.aguirre@protonmail.com>
2021-08-25 18:33:14 -0700

    Snake Initials

---
+++ client.js
@@ -0,0 +1,28 @@
+const net = require('net');
+
+// establishes a connection with the game server
+const connect = () => {
+  const conn = net.createConnection({
+    host: '135.23.223.133',
+    port: '50542',
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data)
+  });
+
+  conn.on('connect', () => {
+    // console.log('Successfully connected to game server!')
+    conn.write('Name: JAG!');
+  });
+
+
+  return conn;
+};
+
+module.exports = {
+  connect
+};
