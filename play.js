f708c6bf559802d12e6ceb37899ce05fe8646f0a
Jairo (Burnaby, BC) <jairo.aguirre@protonmail.com>
2021-08-25 19:28:57 -0700

    Stdin Setup

--- play.js
+++ play.js
@@ -1,5 +1,24 @@
 const {connect} = require('./client');
 
+// setup interface to handle user input from stdin
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
 console.log("Connecting ...");
+
+const handleUserInput = (data) => {
+  // your code here
+};
+
+setupInput();
 
 connect();
